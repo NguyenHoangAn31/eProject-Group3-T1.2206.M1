46daea635f007d88d58f3d7fdcacab610d089c27
An <anphung311002@gmail.com>
2022-10-16 08:44:03 +0700

    updatefile

--- main.js
+++ main.js
@@ -39,8 +39,6 @@ var header = `<div class="header position-fixed top-0 end-0 start-0" style="z-in
 </nav>
 </div>`
 
-
-
 var footer = `<div class="footer">
 <div class="row container m-auto py-5 mt-5">
     <div class="col-lg-3">
@@ -76,8 +74,6 @@ var footer = `<div class="footer">
 <div class="container-fluid text-center">© Copyright 2022 - ZaraTherapyClinic</div>
 </div>`
 
-
-
 $("#header").html(header);
 $("#footer").html(footer);
 
@@ -85,30 +81,95 @@ $("#footer").html(footer);
 AOS.init();
 
 
-
-
 var mang = [
-    { "id": 0,  "imgsrc": "IMGS/homepage/doctor1.jpg",  "name":"product1" ,  "content": "đây là sản phẩm 1"  },
-    { "id": 1,  "imgsrc": "IMGS/homepage/doctor2.jpg",  "name":"product2" ,  "content": "đây là sản phẩm 2"  },
-    { "id": 2,  "imgsrc": "IMGS/homepage/doctor3.jpg",  "name":"product3" ,  "content": "đây là sản phẩm 3"  },
-    { "id": 3,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product4" ,  "content": "đây là sản phẩm 4"  },
-    { "id": 4,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product5" ,  "content": "đây là sản phẩm 5"  },
-    { "id": 5,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product6" ,  "content": "đây là sản phẩm 6"  },
-    { "id": 6,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product7" ,  "content": "đây là sản phẩm 7"  },
-    { "id": 7,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product8" ,  "content": "đây là sản phẩm 8"  },
-    { "id": 8,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product9" ,  "content": "đây là sản phẩm 9"  },
-    { "id": 9,  "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product10" , "content": "đây là sản phẩm 10" },
-    { "id": 10, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product11" , "content": "đây là sản phẩm 11" },
-    { "id": 11, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product12" , "content": "đây là sản phẩm 12" },
-    { "id": 12, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product13" , "content": "đây là sản phẩm 13" },
-    { "id": 13, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product14" , "content": "đây là sản phẩm 14" },
-    { "id": 14, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product15" , "content": "đây là sản phẩm 15" },
-    { "id": 15, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product16" , "content": "đây là sản phẩm 16" },
-    { "id": 16, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product17" , "content": "đây là sản phẩm 17" },
-    { "id": 17, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product18" , "content": "đây là sản phẩm 18" },
-    { "id": 18, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product19" , "content": "đây là sản phẩm 19" },
-    { "id": 19, "imgsrc": "IMGS/homepage/doctor4.jpg",  "name":"product20" , "content": "đây là sản phẩm 20" },
+    { "id": 0, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product1", "content": "đây là sản phẩm 1", "review": 200 },
+    { "id": 1, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product2", "content": "đây là sản phẩm 2", "review": 178 },
+    { "id": 2, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product3", "content": "đây là sản phẩm 3", "review": 347 },
+    { "id": 3, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product4", "content": "đây là sản phẩm 4", "review": 79 },
+    { "id": 4, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product5", "content": "đây là sản phẩm 5", "review": 183 },
+    { "id": 5, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product6", "content": "đây là sản phẩm 6", "review": 249 },
+    { "id": 6, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product7", "content": "đây là sản phẩm 7", "review": 357 },
+    { "id": 7, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product8", "content": "đây là sản phẩm 8", "review": 173 },
+    { "id": 8, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product9", "content": "đây là sản phẩm 9", "review": 346 },
+    { "id": 9, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product10", "content": "đây là sản phẩm 10", "review": 258 },
+    { "id": 10, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product11", "content": "đây là sản phẩm 11", "review": 112 },
+    { "id": 11, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product12", "content": "đây là sản phẩm 12", "review": 76 },
+    { "id": 12, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product13", "content": "đây là sản phẩm 13", "review": 87 },
+    { "id": 13, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product14", "content": "đây là sản phẩm 14", "review": 98 },
+    { "id": 14, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product15", "content": "đây là sản phẩm 15", "review": 154 },
+    { "id": 15, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product16", "content": "đây là sản phẩm 16", "review": 193 },
+    { "id": 16, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product17", "content": "đây là sản phẩm 17", "review": 34 },
+    { "id": 17, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product18", "content": "đây là sản phẩm 18", "review": 74 },
+    { "id": 18, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product19", "content": "đây là sản phẩm 19", "review": 85 },
+    { "id": 19, "pricenew": 20, "priceold": 22, "imgsrc": "IMGS/homepage/doctor1.jpg", "name": "product20", "content": "đây là sản phẩm 20", "review": 53 },
 ]
+
+
+
+var page1 = ""
+for (var i = 0; i < 5; i++) {
+        page1 += `
+        <div class="item" >
+                <div class="card text-center">
+                    <img onclick="productdetail(${mang[i].id})" src="${mang[i].imgsrc}"   width="100%" height="175">
+                    <h3>${mang[i].name}</h3>
+                    <p class="price">$${mang[i].pricenew}</p>
+                    <p>${mang[i].content}</p>
+                    <p><button class="btn btn-primary">Add to Cart</button></p>
+                </div>
+        </div>`
+}
+$("#page1").html(page1)
+
+
+var page2 = ""
+for (var i = 5; i < 10; i++) {
+        page2 += `
+        <div class="item" >
+                <div class="card text-center">
+                    <img onclick="productdetail(${mang[i].id})" src="${mang[i].imgsrc}"   width="100%" height="175">
+                    <h3>${mang[i].name}</h3>
+                    <p class="price">$${mang[i].pricenew}</p>
+                    <p>${mang[i].content}</p>
+                    <p><button class="btn btn-primary">Add to Cart</button></p>
+                </div>
+        </div>`
+}
+$("#page2").html(page2)
+
+
+var page3 = ""
+for (var i = 10; i < 15; i++) {
+        page3 += `
+        <div class="item" >
+                <div class="card text-center">
+                    <img onclick="productdetail(${mang[i].id})" src="${mang[i].imgsrc}"   width="100%" height="175">
+                    <h3>${mang[i].name}</h3>
+                    <p class="price">$${mang[i].pricenew}</p>
+                    <p>${mang[i].content}</p>
+                    <p><button class="btn btn-primary">Add to Cart</button></p>
+                </div>
+        </div>`
+}
+$("#page3").html(page3)
+
+
+var page4 = ""
+for (var i = 15; i < 20; i++) {
+        page4 += `
+        <div class="item" >
+                <div class="card text-center">
+                    <img onclick="productdetail(${mang[i].id})" src="${mang[i].imgsrc}"   width="100%" height="175">
+                    <h3>${mang[i].name}</h3>
+                    <p class="price">$${mang[i].pricenew}</p>
+                    <p>${mang[i].content}</p>
+                    <p><button class="btn btn-primary">Add to Cart</button></p>
+                </div>
+        </div>`
+}
+$("#page4").html(page4)
+
+
 
 
 
@@ -119,9 +180,9 @@ mang.forEach(element => {
     check += `
     <div class="item" >
             <div class="card text-center">
-                <img src="${element.imgsrc}" onclick="productdetail(${element.id})"  width="100%" height="175">
+                <img onclick="productdetail(${element.id})" src="${element.imgsrc}"   width="100%" height="175">
                 <h3>${element.name}</h3>
-                <p class="price">$19.99</p>
+                <p class="price">$${element.pricenew}</p>
                 <p>${element.content}</p>
                 <p><button class="btn btn-primary">Add to Cart</button></p>
             </div>
@@ -129,6 +190,8 @@ mang.forEach(element => {
 });
 
 $("#checkcheck").html(check);
+
+
 
 $('.owl-carousel').owlCarousel({
     loop: true,
@@ -148,14 +211,7 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-
-
-
-
-
-
-
-function productdetail(n){
+function productdetail(n) {
     var indexarr = mang[n];
     var contain = `<div class="productdetail" id="productdetail" style="padding-top:100px">
 
@@ -168,7 +224,7 @@ function productdetail(n){
       <div style="width:500px">
         <div class="h2">${indexarr.name}</div>
         <div class="rating">
-          <span>3 Reviews</span>
+          <span>${indexarr.review} Reviews</span>
           <input type="radio" name="rating" value="5" id="5"><label for="5">☆</label>
           <input type="radio" name="rating" value="4" id="4"><label for="4">☆</label>
           <input type="radio" name="rating" value="3" id="3"><label for="3">☆</label>
@@ -178,8 +234,8 @@ function productdetail(n){
         </div>
 
         <div style="clear: both;">
-          <h4>$00.00</h3>
-            <h6><del>$00.00</del></h5>
+          <h4>$${indexarr.pricenew}</h3>
+            <h6><del>$${indexarr.priceold}</del></h5>
         </div>
         <div>
           ${indexarr.content}
